bca6042593df6c16cc3b06f9ac530be58164a149
useradmin <amutawa@hotmail.com>
2024-11-10 11:09:04 +0300

    components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,8 @@
 rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'react/prop-types': 'off', // add this line
-    'react/no-unescaped-entities': 'off' // add this line
-  },
+  'react-refresh/only-export-components': [
+    'warn',
+    { allowConstantExport: true },
+  ],
+  'react/prop-types': 'off', // add this line
+  'react/no-unescaped-entities': 'off' // add this line
+},
